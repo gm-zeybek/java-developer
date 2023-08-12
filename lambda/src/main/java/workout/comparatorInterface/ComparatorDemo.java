ac6c2550eea166164c3029d9b0b22e614d508f3a
gm-zeybek <gmzeybek@gmail.com>
2023-08-12 23:44:38 +0100

    comparator methods

--- lambda/src/main/java/workout/comparatorInterface/ComparatorDemo.java
+++ lambda/src/main/java/workout/comparatorInterface/ComparatorDemo.java
@@ -1,9 +1,12 @@
 package workout.comparatorInterface;
 
+import javax.sound.midi.Soundbank;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+
+import static java.util.Comparator.comparing;
 
 public class ComparatorDemo {
 
@@ -12,13 +15,16 @@ public class ComparatorDemo {
         System.out.println(list);
         Collections.sort(list);
 
-//        Collections.sort(list,new MyComparator());
-//        System.out.println(list);
-//        Collections.sort(list,((o1, o2) -> (o1>o2)?-1:(o2>o1)?1:0));
-//        System.out.println(list);
+        System.out.println("*****USING COMPARATOR INTERFACE SORT *********");
+        Collections.sort(list,new MyComparator());
+        System.out.println(list);
+        Collections.sort(list,((o1, o2) -> (o1>o2)?-1:(o2>o1)?1:0));
+        System.out.println(list);
         // lambda compareTo method
+        System.out.println("*********USING COMPARE TO METHOD *********");
         list.sort(((o1, o2) -> o1.compareTo(o2)));
         System.out.println(list);
+        System.out.println("*******REVERSED *******");
         list.sort(((o2, o1) -> o1.compareTo(o2)));
         System.out.println(list);
 
@@ -27,8 +33,26 @@ public class ComparatorDemo {
             new Apple(155, Color.RED),
             new Apple(120, Color.GREEN)
         );
-        Comparator<Apple> appleComparator = Comparator.comparing(apple -> apple.getWeight());
+
+        System.out.println("*******USING COMPARATOR METHOD BY STATIC IMPORT********");
+        Comparator<Apple> appleComparator = comparing(apple -> apple.getWeight());
         myInventory.sort(appleComparator);
+        System.out.println(myInventory);
+
+        myInventory.sort(comparing(apple->apple.getWeight()));
+        System.out.println(myInventory);
+        System.out.println("*******DOUBLE COLON OPERATOR ********");
+        myInventory.sort(comparing(Apple::getWeight));
+        // reversed method
+        System.out.println("*******CHAINING DUBLE COLON OPERATOR*******");
+        myInventory.sort((comparing(Apple::getWeight).reversed()));
+        System.out.println(myInventory);
+        // sorting by color
+        myInventory.sort((comparing(Apple::getWeight)));
+        System.out.println(myInventory);
+        // comparing channing
+        System.out.println("********CHAINING MULTIPLE COMPARATOR**********");
+        myInventory.sort(comparing(Apple::getColor).reversed().thenComparing(Apple::getWeight));
         System.out.println(myInventory);
     }
 }
